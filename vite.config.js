b1a10e09e499abf6aec041ad97120577cd5cad25
eyob j <jemijobs22@gmail.com>
2024-12-21 18:16:58 +0300

    fix: fix vite build chunck size limit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
-import glsl from 'vite-plugin-glsl'
+import glsl from 'vite-plugin-glsl';
+import { defineConfig } from 'vite';
 
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
-    },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true, // Add sourcemap
-        target: 'esnext'
-    },
-    plugins:
-    [
-        glsl()
-    ]
-}
+export default defineConfig({
+  plugins: [glsl()],
+  build: {
+    target: 'esnext',
+    chunkSizeWarningLimit: 1600,
+    
+  }
+});
+
